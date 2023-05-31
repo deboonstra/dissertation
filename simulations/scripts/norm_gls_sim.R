817074e73ae9b272995cf79801a27381d57063f5
deboonstra <daniel-boonstra@uiowa.edu>
2023-05-31 14:21:47 -0500

    Updating simulations to include work corr

--- simulations/scripts/norm_gls_sim.R
+++ simulations/scripts/norm_gls_sim.R
@@ -15,7 +15,9 @@ rho <- 0.5
 
 # Simulation ####
 set.seed(1997)
-res <- list()
+res_indp <- list()
+res_cs <- list()
+res_ar1 <- list()
 pb <- txtProgressBar(0, nsims, style = 3)
 for (j in seq_len(nsims)) {
     ## Simulating data ####
@@ -25,22 +27,36 @@ for (j in seq_len(nsims)) {
     ## Produce best subsets
     best_sub <- expand_cols(dat$X)
     # Fitting independence model
-    qic <- rep(NA, length(best_sub))
+    qic1 <- rep(NA, length(best_sub))
+    qic2 <- rep(NA, length(best_sub))
+    qic3 <- rep(NA, length(best_sub))
     for (i in seq_along(best_sub)) {
         XX <- dat$X[, best_sub[[i]]]
         f1 <- geepack::geeglm(yy ~ XX, id = dat$id, corstr = "independence")
-        qic[i] <- unname(geepack::QIC(f1)[1])
+        f2 <- geepack::geeglm(yy ~ XX, id = dat$id, corstr = "exchangeable")
+        f3 <- geepack::geeglm(yy ~ XX, id = dat$id, corstr = "ar1")
+        qic1[i] <- unname(geepack::QIC(f1)[1])
+        qic2[i] <- unname(geepack::QIC(f2)[1])
+        qic3[i] <- unname(geepack::QIC(f3)[1])
     }
     # Determining the best_sub index ####
     # that resulted in the minimum QIC value
-    w <- which.min(qic)
+    w1 <- which.min(qic1)
+    w2 <- which.min(qic2)
+    w3 <- which.min(qic3)
     # Updating res object ####
-    res[[j]] <- list(
-        qic = qic, min = w, qic_min = qic[w], vars = best_sub[[w]]
+    res_indp[[j]] <- list(
+        qic = qic1, min = w1, qic_min = qic1[w1], vars = best_sub[[w1]]
+    )
+    res_cs[[j]] <- list(
+        qic = qic2, min = w2, qic_min = qic2[w2], vars = best_sub[[w2]]
+    )
+    res_ar1[[j]] <- list(
+        qic = qic3, min = w3, qic_min = qic3[w3], vars = best_sub[[w3]]
     )
     setTxtProgressBar(pb, j)
 }
 close(pb)
 
 # Save simulations ####
-saveRDS(res, file = "./simulations/outputs/norm_gls_sim.rds")
+save(res_indp, res_cs, res_ar1, file = "./simulations/outputs/norm_gls_sim.RData")
