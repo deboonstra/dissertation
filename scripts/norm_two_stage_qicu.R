9cc2799914d061a59917eeb6c8f3c65ac5f7194d
deboonstra <daniel-boonstra@uiowa.edu>
2023-09-06 14:08:17 -0500

    Adding QIC based correlation selection

--- scripts/norm_two_stage_qicu.R
+++ scripts/norm_two_stage_qicu.R
@@ -14,13 +14,16 @@ n <- 4
 beta <- c(2.0, 3.0, 0.5, 0, 0, 0)
 rho <- 0.5
 corstr <- "exchangeable"
-work_corstr <- c("exchangeable", "ar1", "unstructured")
+work_corstr <- c("independence", "exchangeable", "ar1", "unstructured")
 
 # Simulation ####
 set.seed(1997)
-res_qic <- list()
-res_qicu <- list()
-res_cic <- list()
+res_cic_corstr <- list()
+res_qic_corstr <- list()
+res_cic_qic <- list()
+res_cic_qicu <- list()
+res_qic_qic <- list()
+res_qic_qicu <- list()
 pb <- utils::txtProgressBar(0, nsims, style = 3)
 for (j in seq_len(nsims)) {
   ## Simulating data ####
@@ -31,19 +34,28 @@ for (j in seq_len(nsims)) {
 
   ## Correlation structure selection ####
   cic_corstr <- rep(NA, length(work_corstr))
+  qic_corstr <- rep(NA, length(work_corstr))
   for (k in seq_along(work_corstr)) {
     fit_corstr <- geepack::geeglm(y ~ X, id = id, corstr = work_corstr[k])
     cic_corstr[k] <- cic(fit_corstr)
+    qic_corstr[k] <- qic(fit_corstr)
   }
 
   ### Determining correlation structure ####
-  ### that resulted in the minimum CIC value
-  w_c <- which.min(cic_corstr)
-  best_corstr <- work_corstr[w_c]
+  ### that resulted in the minimum CIC and QIC values
+  w_c_corstr <- which.min(cic_corstr)
+  w_q_corstr <- which.min(qic_corstr)
+  cic_best_corstr <- work_corstr[w_c_corstr]
+  qic_best_corstr <- work_corstr[w_q_corstr]
 
   ### Updating results ####
-  res_cic[[j]] <- list(
-    ic = cic_corstr, ic_min = cic_corstr[w_c], corstr = best_corstr
+  res_cic_corstr[[j]] <- list(
+    ic = cic_corstr, work_corstr = work_corstr,
+    ic_min = cic_corstr[w_c_corstr], corstr = cic_best_corstr
+  )
+  res_qic_corstr[[j]] <- list(
+    ic = qic_corstr, work_corstr = work_corstr,
+    ic_min = qic_corstr[w_q_corstr], corstr = qic_best_corstr
   )
 
   ## Mean structure selection ####
@@ -52,33 +64,61 @@ for (j in seq_len(nsims)) {
   best_sub <- expand_cols(X)
 
   ### Fitting models ####
-  q <- rep(NA, length(best_sub)) # qic
-  qu <- rep(NA, length(best_sub)) # qicu
-  penalty_q <- rep(NA, length(best_sub))
-  penalty_qu <- rep(NA, length(best_sub))
-  gf <- rep(NA, length(best_sub)) # goodness-of-fit
+  c_q <- rep(NA, length(best_sub)) # cic and qic
+  c_qu <- rep(NA, length(best_sub)) # cic and qicu
+  q_q <- rep(NA, length(best_sub)) # qic and qic
+  q_qu <- rep(NA, length(best_sub)) # qic and qicu
+  penalty_c_q <- rep(NA, length(best_sub))
+  penalty_c_qu <- rep(NA, length(best_sub))
+  penalty_q_q <- rep(NA, length(best_sub))
+  penalty_q_qu <- rep(NA, length(best_sub))
+  gf_c <- rep(NA, length(best_sub)) # goodness-of-fit for cic
+  gf_q <- rep(NA, length(best_sub)) # goodness-of-fit for qic
   for (i in seq_along(best_sub)) {
     XX <- X[, best_sub[[i]]]
-    fit_mean <- geepack::geeglm(y ~ XX, id = id, corstr = best_corstr)
-    q[i] <- qic(fit_mean)
-    qu[i] <- qicu(fit_mean)
-    penalty_q[i] <- 2 * cic(fit_mean)
-    penalty_qu[i] <- 2 * nparms(fit_mean)
-    gf[i] <- gof(fit_mean)
+    fit_mean_c <- geepack::geeglm(y ~ XX, id = id, corstr = cic_best_corstr)
+    fit_mean_q <- geepack::geeglm(y ~ XX, id = id, corstr = qic_best_corstr)
+    #### Obtaining information criterion values
+    ##### CIC based
+    c_q[i] <- qic(fit_mean_c)
+    c_qu[i] <- qicu(fit_mean_c)
+    penalty_c_q[i] <- 2 * cic(fit_mean_c)
+    penalty_c_qu[i] <- 2 * nparms(fit_mean_c)
+    gf_c[i] <- gof(fit_mean_c)
+    ##### QIC based
+    q_q[i] <- qic(fit_mean_q)
+    q_qu[i] <- qicu(fit_mean_q)
+    penalty_q_q[i] <- 2 * cic(fit_mean_q)
+    penalty_q_qu[i] <- 2 * nparms(fit_mean_q)
+    gf_q[i] <- gof(fit_mean_q)
   }
   ### Determining mean structure ####
   ### that resulted in the minimum QIC or QICu values
-  w_q <- which.min(q)
-  w_qu <- which.min(qu)
+  w_c_q <- which.min(c_q)
+  w_c_qu <- which.min(c_qu)
+  w_q_q <- which.min(q_q)
+  w_q_qu <- which.min(q_qu)
 
   ## Updating results ####
-  res_qic[[j]] <- list(
-    ic = qic, gof = gf, penalty = penalty_q,
-    min = w_q, ic_min = q[w_q], vars = best_sub[[w_q]]
+  res_cic_qic[[j]] <- list(
+    ic = c_q, gof = gf_c, penalty = penalty_c_q,
+    min = w_c_q, ic_min = c_q[w_c_q], vars = best_sub[[w_c_q]],
+    corstr = cic_best_corstr
   )
-  res_qicu[[j]] <- list(
-    ic = qicu, gof = gf, penalty = penalty_qu,
-    min = w_qu, ic_min = qu[w_qu], vars = best_sub[[w_qu]]
+  res_cic_qicu[[j]] <- list(
+    ic = c_qu, gof = gf_c, penalty = penalty_c_qu,
+    min = w_c_qu, ic_min = c_qu[w_c_qu], vars = best_sub[[w_c_qu]],
+    corstr = cic_best_corstr
+  )
+  res_qic_qic[[j]] <- list(
+    ic = q_q, gof = gf_q, penalty = penalty_q_q,
+    min = w_q_q, ic_min = q_q[w_q_q], vars = best_sub[[w_q_q]],
+    corstr = qic_best_corstr
+  )
+  res_qic_qicu[[j]] <- list(
+    ic = q_qu, gof = gf_q, penalty = penalty_q_qu,
+    min = w_q_qu, ic_min = q_qu[w_q_qu], vars = best_sub[[w_q_qu]],
+    corstr = qic_best_corstr
   )
 
   ## Updating progress bar ####
@@ -94,6 +134,8 @@ if (!dir.exists("./outputs/norm_two_stage_qicu")) {
 }
 
 save(
-  res_cic, res_qic, res_qicu,
+  res_cic_corstr, res_qic_corstr,
+  res_cic_qic, res_cic_qicu,
+  res_qic_qic, res_qic_qicu,
   file = "./outputs/norm_two_stage_qicu/norm_two_stage_qicu.RData"
 )
