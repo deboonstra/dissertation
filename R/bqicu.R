6f19dffcca13d307af34956bd6733ac665a64d14
deboonstra <daniel-boonstra@uiowa.edu>
2023-09-14 09:42:01 -0500

    Adding modified argument and code to CIC

--- R/bqicu.R
+++ R/bqicu.R
@@ -4,82 +4,94 @@
 
 # quasilik ####
 quasilik <- function(object) {
-    # checking object type
-    if ("geeglm" %notin% class(object)) {
-        stop("bqicu requires a geeglm object as input.")
-    }
-    # outputting quasi-likelihood
-    unname(geepack::QIC(object)[3])
+  # checking object type
+  if (!("geeglm" %in% class(object))) {
+    stop("object must be a geepack::geeglm object.")
+  }
+  # outputting quasi-likelihood
+  unname(geepack::QIC(object)[3])
 }
 
 # nparms ####
 nparms <- function(object) {
-    # checking object type
-    if ("geeglm" %notin% class(object)) {
-        stop("bqicu requires a geeglm object as input.")
-    }
-    # outputting number of parameters
-    unname(geepack::QIC(object)[5])
+  # checking object type
+  if (!("geeglm" %in% class(object))) {
+    stop("object must be a geepack::geeglm object.")
+  }
+  # outputting number of parameters
+  unname(geepack::QIC(object)[5])
 }
 
 # gof ####
 gof <- function(object) {
-    # checking object type
-    if ("geeglm" %notin% class(object)) {
-        stop("bqicu requires a geeglm object as input.")
-    }
-    # outputing goodness-of-fit
-    -2 * quasilik(object)
+  # checking object type
+  if (!("geeglm" %in% class(object))) {
+    stop("object must be a geepack::geeglm object.")
+  }
+  # outputing goodness-of-fit
+  -2 * quasilik(object)
 }
 
 # qic ####
 qic <- function(object) {
-    # checking object type
-    if ("geeglm" %notin% class(object)) {
-        stop("bqicu requires a geeglm object as input.")
-    }
-    # outputting qic
-    unname(geepack::QIC(object)[1])
+  # checking object type
+  if (!("geeglm" %in% class(object))) {
+    stop("object must be a geepack::geeglm object.")
+  }
+  # outputting qic
+  unname(geepack::QIC(object)[1])
 }
 
 # qicu ####
 qicu <- function(object) {
-    # checking object type
-    if ("geeglm" %notin% class(object)) {
-        stop("bqicu requires a geeglm object as input.")
-    }
-    # outputting qicu
-    unname(geepack::QIC(object)[2])
+  # checking object type
+  if (!("geeglm" %in% class(object))) {
+    stop("object must be a geepack::geeglm object.")
+  }
+  # outputting qicu
+  unname(geepack::QIC(object)[2])
 }
 
 # cic ####
-cic <- function(object) {
-    # checking object type
-    if ("geeglm" %notin% class(object)) {
-        stop("bqicu requires a geeglm object as input.")
-    }
-    # outputing cic
-    unname(geepack::QIC(object)[4])
+cic <- function(object, modified = FALSE) {
+  # checking object type
+  if (!("geeglm" %in% class(object))) {
+    stop("object must be a geepack::geeglm object.")
+  }
+
+  if (!is.logical(modified)) {
+    stop("modified must be a logical value.")
+  }
+  
+  # outputing cic
+  if (!modified) {
+    cic <- unname(geepack::QIC(object)[4])
+  } else {
+    vr <- object$geese$vbeta
+    omega <- solve(object$geese$vbeta.naiv)
+    cic <- sum(diag(omega %*% vr))
+  }
+  return(cic)
 }
 
 # qicc ####
 qicc <- function(object) {
-    # checking object type
-    if ("geeglm" %notin% class(object)) {
-        stop("bqicu requires a geeglm object as input.")
-    }
-    # outputting ####
-    unname(geepack::QIC(object)[6])
+  # checking object type
+  if (!("geeglm" %in% class(object))) {
+    stop("object must be a geepack::geeglm object.")
+  }
+  # outputting ####
+  unname(geepack::QIC(object)[6])
 }
 
 # bqicu ####
 bqicu <- function(object) {
-    # checking object type
-    if ("geeglm" %notin% class(object)) {
-        stop("bqicu requires a geeglm object as input.")
-    }
+  # checking object type
+  if (!("geeglm" %in% class(object))) {
+    stop("object must be a geepack::geeglm object.")
+  }
 
-    # calculating bqicu
-    n <- length(unique(object$id))
-    gof(object) + (log(n) * nparms(object))
+  # calculating bqicu
+  n <- length(unique(object$id))
+  gof(object) + (log(n) * nparms(object))
 }
