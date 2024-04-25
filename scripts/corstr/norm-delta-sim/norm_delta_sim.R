4d5e53f72bd046bb747a22e73d3f99052e76ee70
deboonstra <daniel-boonstra@uiowa.edu>
2024-04-25 09:32:04 -0500

    Adding oracle values to simulation results

--- scripts/corstr/norm-delta-sim/norm_delta_sim.R
+++ scripts/corstr/norm-delta-sim/norm_delta_sim.R
@@ -50,6 +50,106 @@ res_basis <- data.frame(
   )
 )
 
+# Obtaining the true covariance matrix ####
+set.seed(1997)
+nsims_cov <- nsims * 10L
+D <- 0 # nolint: Reducing nsims_cov if diag(mb) < 0
+sigma0 <- vector(mode = "list", length = nrow(res_basis))
+names(sigma0) <- paste0(
+  "N", res_basis$N, "n", res_basis$n, "corstr_", res_basis$corstr
+)
+for (ell in seq_along(sigma0)) {
+  sigma0[[ell]] <- matrix(
+    data = 0,
+    nrow = length(which(beta != 0)) + 1,
+    ncol = length(which(beta != 0)) + 1
+  )
+  cat(
+    paste0(
+      "Simulations for N = ",
+      res_basis$N[ell],
+      " and n = ",
+      res_basis$n[ell],
+      " given corstr = ",
+      res_basis$corstr[ell],
+      "\n"
+    )
+  )
+  pb <- utils::txtProgressBar(
+    min = 0, max = nsims_cov, style = 3, initial = "\n"
+  )
+  for (i in seq_len(nsims_cov)) {
+    # Simulating data ####
+    dat <- sim_data(
+      N = res_basis$N[ell], n = res_basis$n[ell], beta = beta, rho = rho,
+      corstr = res_basis$corstr[ell]
+    )
+    ## Converting to data.frame ####
+    dat <- data.frame(y = dat$y, dat$X, id = dat$id)
+
+    # Fitting model ####
+    fit <- gee::gee(
+      formula = form, id = id,
+      data = dat,
+      corstr = ifelse(
+        test = res_basis$corstr[ell] == "ar1",
+        yes = "AR-M",
+        no = res_basis$corstr[ell]
+      )
+    )
+
+    ## Pulling model-based covariance matrix ####
+    mb <- fit$naive.variance
+
+    ### Obtaining new covariance matrice if diag(mb) < 0 ####
+    iter <- 0
+    while (sum(diag(mb) < 0) >= 1 && iter <= 100) {
+      # Simulating data ####
+      dat <- sim_data(
+        N = res_basis$N[ell], n = res_basis$n[ell], beta = beta, rho = rho,
+        corstr = res_basis$corstr[ell]
+      )
+      ## Converting to data.frame ####
+      dat <- data.frame(y = dat$y, dat$X, id = dat$id)
+
+      # Fitting model ####
+      fit <- gee::gee(
+        formula = form, id = id,
+        data = dat,
+        corstr = ifelse(
+          test = res_basis$corstr[ell] == "ar1",
+          yes = "AR-M",
+          no = res_basis$corstr[ell]
+        )
+      )
+
+      ## Pulling model-based covariance matrix ####
+      mb <- fit$naive.variance
+
+      # Updating iteration count ####
+      iter <- iter + 1
+    }
+
+    # Updating reduction variable ####
+    if (iter == 100 && sum(diag(mb) < 0) >= 1) {
+      diag(mb) <- matrix(data = 0, nrow = dim(mb)[1], nrol = dim(mb)[2])
+      D <- D + 1 # nolint
+    }
+
+    # Summing model-based covariance matrices ####
+    sigma0[[ell]] <- sigma0[[ell]] + mb
+
+    # Updating progress bar ####
+    utils::setTxtProgressBar(pb, i)
+    if (i == nsims_cov) {
+      close(pb)
+      cat("\n")
+    }
+  }
+  # Averaging model-based covariance matrices ####
+  sigma0[[ell]] <- (1 / (nsims_cov - D)) * sigma0[[ell]]
+}
+
 # Simulation for delta values ####
 set.seed(1997)
 res <- vector(mode = "list", length = nrow(res_basis))
@@ -72,7 +172,7 @@ for (ell in seq_len(nrow(res_basis))) {
     cl = cl,
     X = seq_len(res_basis$sims[ell]),
     fun = function(j) {
-      # Simulation data ####
+      # Simulating data ####
       dat <- sim_data(
         N = res_basis$N[ell], n = res_basis$n[ell], beta = beta, rho = rho,
         corstr = res_basis$corstr[ell]
@@ -82,7 +182,8 @@ for (ell in seq_len(nrow(res_basis))) {
       dat <- data.frame(y = dat$y, dat$X, id = dat$id)
 
       # Fitting models and getting delta values ####
-      dd <- rep(NA, times = length(work_corstr))
+      dd <- rep(NA, times = length(work_corstr)) ## statistics
+      dd0 <- NA ## oracle
       names(dd) <- names_work_corstr
       for (k in seq_along(work_corstr)) {
         ## Fitting model ####
@@ -141,6 +242,16 @@ for (ell in seq_len(nrow(res_basis))) {
         }
       }
 
+      ## Obtaining the oracle delta ####
+      ## based on the minimum delta statistic
+      fit0 <- gee::gee(
+        formula = form, id = id,
+        data = dat,
+        corstr = work_corstr[which.min(dd)],
+        Mv = mv[which.min(dd)]
+      )
+      dd0 <- delta(a = fit0$naive.variance, b = sigma0[[ell]])
+
       # Comparison of delta values ####
       # There will be four comparisons, which are outlined below.
       # 1. Exchangeable and AR(1)
@@ -195,6 +306,7 @@ for (ell in seq_len(nrow(res_basis))) {
           nrow = 1, ncol = length(work_corstr),
           dimnames = list(NULL, names_work_corstr), byrow = TRUE
         ),
+        delta0 = dd0,
         sel1 = sel1,
         sel2 = sel2,
         sel3 = sel3,
