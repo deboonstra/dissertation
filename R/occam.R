2b34d1ede83ed52a731b45746e4e084c630883c6
deboonstra <daniel-boonstra@uiowa.edu>
2024-06-25 11:14:55 -0500

    Fixing incorrect selection of minimum value when strict_upper is FALSE

--- R/occam.R
+++ R/occam.R
@@ -16,11 +16,17 @@
 # options pre
 # mv, When x contains "stat_M_dep", "non_stat_M_dep", or "AR-M" then mv must be
 # specified and the same length as x.
+# strict_upper, Logical indicating if a strict upper bound should be used. If
+# TRUE then only the values below the upper bound are considered and the
+# resulting implementation of Occam's window could select none of the
+# structures. If FALSE, then the structure with minimum value will be selected
+# if all of the values fall outside of the Occam's window.
 
 # Details: If length(window) ==  2 then the value of x closet in absolute value
 # to ideal is choosen.
 
-occam <- function(x, ideal, window = 1, nparms = NULL, mv = 1) {
+occam <- function(
+    x, ideal, window = 1, nparms = NULL, mv = 1, strict_upper = TRUE) {
   # Checking parameter values ####
   if (!is.numeric(x) || is.null(names(x))) {
     stop("x must be a named numeric vector.")
@@ -45,12 +51,51 @@ occam <- function(x, ideal, window = 1, nparms = NULL, mv = 1) {
       stop("mv must be a numeric vector the same length as x.")
     }
   }
+  if (!is.logical(strict_upper)) {
+    stop("strict_upper must be a logical.")
+  }
+
+  # Finding the number of parameters
+
+  ## mv ####
+  if (sum(names(x) %in% mv_names) > 0) {
+    mv <- mv
+  }
+
+  ##  Number of parameters estimated ####
+  if (!is.null(nparms)) {
+    nparms <- nparms
+  } else {
+    nparms <- rep(NA, times = length(x))
+    ### Independence ####
+    if ("independence" %in% names(x)) {
+      w_nparms <- which(names(x) == "independence")
+      nparms[w_nparms] <- 0
+    }
+
+    ### Exchangeable ####
+    if ("exchangeable" %in% names(x)) {
+      w_nparms <- which(names(x) == "exchangeable")
+      nparms[w_nparms] <- 1
+    }
+
+    ### AR-M, Stationary and non-statationary M-dependent ####
+    if (sum(names(x) %in% mv_names) > 0) {
+      w_nparms <- which(names(x) %in% mv_names)
+      nparms[w_nparms] <- mv[w_nparms]
+    }
+
+    ### Unstructured ####
+    if ("unstructured" %in% names(x)) {
+      w_nparms <- which(names(x) == "unstructured")
+      nparms[w_nparms] <- (ideal * (ideal - 1)) / 2
+    }
+  }
+
 
   # Implementing Occam's window ####
 
-  ## Subsetting x for the values within window ####
-
-  ### Determining which values within window ####
+  ## Determining which values within window ####
   if (length(window) == 1) {
     w <- which(x <= (ideal + window))
   } else if (length(window) == 2) {
@@ -63,70 +108,33 @@ occam <- function(x, ideal, window = 1, nparms = NULL, mv = 1) {
     w <- which(x >= ideal + window[1] & x <= ideal + window[2])
   }
 
+  ## Selecting value with minimum number of parameters ####
   if (length(w) > 0) {
-    ### Subsetting ####
-    #### Named vector of values to select from ####
+    ### Selecting values within window ####
     x_wind <- x[w]
+    nparms_wind <- nparms[w]
 
-    #### Number of parameters estimated ####
-    if (!is.null(nparms)) {
-      nparms <- nparms[w]
-    }
-
-    #### mv ####
-    if (sum(names(x) %in% mv_names) > 0) {
-      mv <- mv[w]
-    }
-
-
-    ## Calculating number of parameters estimated ####
-    if (is.null(nparms)) {
-      ### Independence ####
-      if ("independence" %in% names(x_wind)) {
-        w_nparms <- which(names(x_wind) == "independence")
-        nparms[w_nparms] <- 0
-      }
-
-      ### Exchangeable ####
-      if ("exchangeable" %in% names(x_wind)) {
-        w_nparms <- which(names(x_wind) == "exchangeable")
-        nparms[w_nparms] <- 1
-      }
-
-      ### AR-M, Stationary and non-statationary M-dependent ####
-      if (sum(names(x_wind) %in% mv_names) > 0) {
-        w_nparms <- which(names(x_wind) %in% mv_names)
-        nparms[w_nparms] <- mv[w_nparms]
-      }
-
-      ### Unstructured ####
-      if ("unstructured" %in% names(x_wind)) {
-        w_nparms <- which(names(x_wind) == "unstructured")
-        nparms[w_nparms] <- (ideal * (ideal - 1)) / 2
-      }
-    }
-
-    ## Selecting value with minimum number of parameters ####
-    w_sel <- which(nparms == min(nparms))
+    ### Selecting the value with the minimum number of parameters ####
+    w_sel <- which(nparms_wind == min(nparms_wind))
     if (length(w_sel) == 1) {
       x_sel <- x_wind[w_sel]
-      nparms_sel <- nparms[w_sel]
+      nparms_sel <- nparms_wind[w_sel]
     } else if (length(w_sel) > 1) {
       if (length(window) == 1) {
         x_sel <- x_wind[w_sel]
-        nparms_sel <- nparms[w_sel]
+        nparms_sel <- nparms_wind[w_sel]
         x_sel <- x_sel[which(x_sel == min(x_sel))]
         nparms_sel <- nparms_sel[which(x_sel == min(x_sel))]
       } else if (length(window) == 2) {
         x_sel <- x_wind[w_sel]
-        nparms_sel <- nparms[w_sel]
+        nparms_sel <- nparms_wind[w_sel]
         abs_diff <- abs(x_sel - ideal)
-        x_sel <- x_sel[which.min(abs_diff)]
-        nparms_sel <- nparms_sel[which.min(abs_diff)]
+        x_sel <- x_sel[which(abs_diff == min(abs_diff))]
+        nparms_sel <- nparms_sel[which(abs_diff == min(abs_diff))]
       }
     }
 
-    # Creating output data frame ####
+    ### Creating output data frame given at least one value in window ####
     out <- data.frame(
       name = names(x_sel),
       value = unname(x_sel),
@@ -134,13 +142,25 @@ occam <- function(x, ideal, window = 1, nparms = NULL, mv = 1) {
       nparms = unname(nparms_sel)
     )
   } else {
-    # Creating output data frame ####
-    out <- data.frame(
-      name = NA,
-      value = NA,
-      ideal = unname(ideal),
-      nparms = NA
-    )
+    ### Creating output data frame given no value in window and
+    ### strict upper is TRUE resulting in NO structure selected ####
+    if (strict_upper) {
+      out <- data.frame(
+        name = NA,
+        value = NA,
+        ideal = unname(ideal),
+        nparms = NA
+      )
+    } else {
+      ### Creating output data frame given no value in window and ####
+      ### strict upper bound is FALSE resulting in minimum value selected
+      out <- data.frame(
+        name = names(x)[which(x == min(x))],
+        value = x[which(x == min(x))],
+        ideal = unname(ideal),
+        nparms = nparms[which(x == min(x))]
+      )
+    }
   }
 
   # Returning data frame ####
