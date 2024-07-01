352888033ed6cf9daedccc20123bab01d71e3105
deboonstra <daniel-boonstra@uiowa.edu>
2024-07-01 10:30:02 -0500

    Suppressing startup messages and adding stats and utils

--- R/packages.R
+++ R/packages.R
@@ -1,12 +1,17 @@
 # The function of this script file is to load the required packages to run 
 # simulations.
-
-library(dplyr)
-library(Matrix)
-library(expm)
-library(geepack)
-library(gee)
-library(mvtnorm)
-library(knitr)
-library(kableExtra)
-library(rmarkdown)
+suppressMessages(
+  expr = {
+    library(dplyr)
+    library(Matrix)
+    library(expm)
+    library(geepack)
+    library(gee)
+    library(mvtnorm)
+    library(knitr)
+    library(kableExtra)
+    library(rmarkdown)
+    library(stats)
+    library(utils)
+  }
+)
